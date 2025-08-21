1a4b955eb6d74dbb267e0948ab04d5a087fba64a
Diego Reategui <93934696+PeerDiego@users.noreply.github.com>
2025-08-21 12:25:07 -0400

    Update silent-tester-runner-windows.ps1

--- provisional/silent-tester-runner-windows.ps1
+++ provisional/silent-tester-runner-windows.ps1
@@ -1,3 +1,4 @@
 # File Moved
 
-This file has been moved to the root of this repo: /silent-tester-runner-windows.ps1
+This file has been promoted to recommended status, and moved to the root of this repo. 
+https://github.com/PeerDiego/silent-test-script/blob/main/silent-tester-runner-windows.ps1
